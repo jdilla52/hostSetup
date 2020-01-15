8ee5ae8f7e83f36bd5bbc9652bf8fb8e33eba1a4
jacques Perrault <jperrault52@gmail.com>
2020-01-14 23:10:03 -0800

    initial setup
    made pip install able repo
    set up some mock directories

---
+++ setup.py
@@ -0,0 +1,22 @@
+import io
+import re
+from setuptools import setup, find_packages
+
+# Read in requirements.txt
+with open("requirements.txt", "r") as f_requirements:
+    requirements = f_requirements.readlines()
+requirements = [r.strip() for r in requirements]
+
+with io.open("src/host_setup/__init__.py", "rt", encoding="utf8") as f:
+    version = re.search(r"__version__ = \"(.*?)\"", f.read()).group(1)
+    print(version)
+
+setup(
+    name="host setup",
+    version=version,
+    author="Jacques Perrault",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=requirements,
+    zip_safe=False,
+)
---
+++ tests/functional/test_sdf.py
@@ -0,0 +1,6 @@
+import os 
+
+# content of test_class.py
+def test_setup(test_data_dir):
+    print(test_data_dir)
+    assert test_data_dir
