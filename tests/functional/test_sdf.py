3f9cdaf2b367494bdf9ad10f044c01151185a177
jacques Perrault <jperrault52@gmail.com>
2020-03-14 15:21:21 -0700

    cleaned up testing data
    cleaned up tests and test data

--- tests/functional/test_sdf.py
+++ tests/functional/test_sdf.py
@@ -1,10 +1,12 @@
 import os
 from host_setup.actions import ScanDir
+from host_setup.datab import DataB
 import pytest
+
 
 def test_find_downloads(test_data_dir):
     path = os.path.join(test_data_dir, "downloads/")
-    db_path = os.path.join(test_data_dir, "output/db.db")
-    print(path)
+    db_path = os.path.join(test_data_dir, "output/functional_db.db")
+
     scan = ScanDir(path, api=db_path)
     assert test_data_dir
--- tests/unit/test_state_manager.py
+++ tests/unit/test_state_manager.py
@@ -1,11 +1,12 @@
 import os
 from host_setup.datab import DataB
 from host_setup.process import state_manager, logger
+from host_setup.video_dir import VideoDir
 import pytest
 
 
 def test_create_connection(test_data_dir):
-    path = os.path.join(test_data_dir, "output/db.db")
+    path = os.path.join(test_data_dir, "output/decoators.db")
     db = DataB(path)
 
 
@@ -16,26 +17,20 @@ def test_state_manager(test_data_dir):
     db.create_new_table()
     db.create_new_task("hello")
 
-    @state_manager(db, "hello")
-    def dumb_def(hello):
-        print(hello)
+    @state_manager
+    def dumb_def(video, api):
+        print("hello")
         print()
 
-    dumb_def("sdfsdf")
-    db.delete_database()
+    j = VideoDir(path)
+    dumb_def(j, db)
+    # db.delete_database()
 
 
 def test_logging(test_data_dir):
-    path = os.path.join(test_data_dir, "downloads/movie2")
-    db_path = os.path.join(test_data_dir, "test3.db")
-    db = DataB(db_path)
-    db.create_new_table()
-    db.create_new_task("hello")
-
     @logger
     def dumb_def(hello):
         print(hello)
         print()
 
     dumb_def("sdfsdf")
-    db.delete_database()
