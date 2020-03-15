160afaed7bcd97758d7b3ebfea93a09b46dcbba6
jacques Perrault <jperrault52@gmail.com>
2020-03-15 14:10:07 -0700

    create base media class
    setting up to deal with video, tv, music, other

---
+++ src/host_setup/media/base_media.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod, abstractstaticmethod, abstractproperty
+from typing import List
+from pathlib import Path
+from glob import glob
+import os
+from dataclasses import dataclass
+
+class BaseMedia(ABC):
+    def __init__(self, path:str, settings:MediaTarget):
+        self.path = path
+        self.settings = settings
+
+    @abstractmethod
+    def __repr__(self):
+        pass
+
+    @abstractmethod
+    def process_media(self):
+        pass
+
+    @property
+    def name(self):
+        return os.path.basename(self.path)
+
+    @property
+    def r_path(self):
+        return str(self.path)
+    
+    @staticmethod
+    def _get_files(path: str, file_types: List[str]) -> List[str]:
+        return [p for p in Path(path).rglob("*") if p.suffix in file_types]
+
